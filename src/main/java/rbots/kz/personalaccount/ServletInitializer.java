96c1772a668dff53c5a28ea98576cdce7d47cff8
BaLiviya <bal_mc@mail.ru>
2017-08-13 11:52:35 +0600

    create repo

---
+++ src/main/java/rbots/kz/personalaccount/ServletInitializer.java
@@ -0,0 +1,13 @@
+package rbots.kz.personalaccount;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PersonalAccountApplication.class);
+	}
+
+}
