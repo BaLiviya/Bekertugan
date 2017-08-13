cf0a16fe4bd70009b1e702aa43edd7a5b117a01c
BaLiviya <bal_mc@mail.ru>
2017-08-13 16:34:28 +0600

    Create class User, Role for User (Security Spring), create SQL script )))

---
+++ src/main/java/rbots/kz/personalaccount/jpa/User.java
@@ -0,0 +1,33 @@
+package rbots.kz.personalaccount.jpa;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    @ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
