b1d2bacd0ae25d38af422da191c366aa618e6e02
BaLiviya <bal_mc@mail.ru>
2017-08-15 00:08:58 +0600

    rename UserService, create SecurityService, UserDetailService, update gitignore

--- src/main/java/rbots/kz/SecurityService/service/UserService.java
+++ src/main/java/rbots/kz/SecurityService/service/UserService.java
@@ -1,13 +1,11 @@
 package rbots.kz.SecurityService.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import rbots.kz.SecurityService.dao.RoleRepository;
 import rbots.kz.SecurityService.dao.UserRepository;
 import rbots.kz.SecurityService.jpa.Role;
 import rbots.kz.SecurityService.jpa.User;
@@ -16,19 +14,28 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    @Override
-    @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        for (Role role : user.getRoles()) {
-            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
-        }
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Bean
+    private PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder().encode(user.getPassword()));
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleRepository.getOne(1));
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
