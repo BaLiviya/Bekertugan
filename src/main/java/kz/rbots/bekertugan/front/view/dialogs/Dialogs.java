ac01dafdadec0723085210720c8197e8620b4596
Bartosso <barton547@gmail.com>
2018-03-08 20:16:28 +0800

    Now you can navigate into dialog and pm to that faggots, also you can return to dialog list

--- src/main/java/kz/rbots/bekertugan/front/view/dialogs/Dialogs.java
+++ src/main/java/kz/rbots/bekertugan/front/view/dialogs/Dialogs.java
@@ -1,5 +1,7 @@
 package kz.rbots.bekertugan.front.view.dialogs;
 
+import com.vaadin.event.Action;
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.server.ExternalResource;
 import com.vaadin.shared.Registration;
 import com.vaadin.ui.*;
@@ -7,24 +9,26 @@ import kz.rbots.bekertugan.broadcaster.Broadcaster;
 import kz.rbots.bekertugan.entities.Dialog;
 import kz.rbots.bekertugan.front.data.mock.DummyDialogData;
 import kz.rbots.bekertugan.telegrambot.TelegramBot;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Dialogs extends Panel implements Broadcaster.TelegramDialogsUpdateListener {
-    private VerticalLayout dialogs = new VerticalLayout();
+    private VerticalLayout dialogsLayout = new VerticalLayout();
     private boolean listIsEmpty;
-    private List<Dialog> dialogList = DummyDialogData.getAllDialogs().collect(Collectors.toList());
+    private boolean inChatWindow;
     public Dialogs() {
         super();
         DummyDialogData.getAllDialogs().forEach(x->addNewDialog(x, TelegramBot.getToken()));
         this.setSizeFull();
-        if (dialogs.getComponentCount()==0){
-            Label noDialogs = new Label("Here is no dialogs");
+        if (dialogsLayout.getComponentCount()==0){
+            Label noDialogs = new Label("Here is no dialogsLayout");
             listIsEmpty = true;
             setContent(noDialogs);
         } else {
-            setContent(dialogs);
+            setContent(dialogsLayout);
         }
         Broadcaster.registerDialogListener(this);
     }
@@ -42,25 +46,87 @@ public class Dialogs extends Panel implements Broadcaster.TelegramDialogsUpdateL
 
     @Override
     public void receiveBroadcast(Dialog dialog, String botToken) {
-        System.out.println("HaveBroadcast");
-        if (listIsEmpty){
-            listIsEmpty = false;
-            addNewDialog(dialog,botToken);
-            setContent(dialogs);
-        } else {
-            addNewDialog(dialog,botToken);
+        if (!inChatWindow) {
+            System.out.println("HaveBroadcast");
+            if (listIsEmpty) {
+                listIsEmpty = false;
+                addNewDialog(dialog, botToken);
+                setContent(dialogsLayout);
+            } else {
+                addNewDialog(dialog, botToken);
+            }
+            getUI().access(() -> dialogsLayout.addComponent(new Label()));
         }
-        getUI().access(()-> dialogs.addComponent(new Label()));
     }
 
     private void addNewDialog(Dialog dialog,String botToken){
+        VerticalLayout dialogLayout = new VerticalLayout();
         ExternalResource externalResource = new ExternalResource("https://api.telegram.org/file/bot"
         + botToken +"/" + dialog.getAvatarFileId());
-//        ExternalResource externalResource = new ExternalResource(dialog.getAvatarFileId());
+        dialogLayout.setId(String.valueOf(dialog.getChatId()));
         Image image = new Image(dialog.getNameAndLastname(),externalResource);
-        image.setHeight("50px");
-        image.setWidth("50px");
-        dialogs.addComponents(image);
-        dialogs.addComponent(new Button("@"+dialog.getUserName()));
+        image.setHeight("100px");
+        image.setWidth("100px");
+        Button dialogButton = new Button("@"+dialog.getUserName());
+        dialogButton.setId(String.valueOf(dialog.getChatId()));
+        dialogButton.addClickListener(this::loginButtonClick);
+        dialogLayout.addComponents(image,dialogButton);
+        dialogsLayout.addComponents(dialogLayout);
+    }
+
+    private void loginButtonClick(Button.ClickEvent e) {
+        String chatId = e.getButton().getId();
+        getUI().access(()->setContent(new ChatWindow(chatId)));
+        Broadcaster.unregisterDialogsListener(this);
+    }
+
+    private class ChatWindow extends VerticalLayout implements Broadcaster.BotUpdatesListener {
+        private final String chatId;
+        private final TextField textField = new TextField("Type text here: ");
+        Button sendButton = new Button("Send Message");
+        Button backToDialogs = new Button("Return");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        HorizontalLayout enterTextLayout = new HorizontalLayout();
+
+
+        private ChatWindow(String chatId) {
+
+            this.chatId = chatId;
+            sendButton.addClickListener(e-> {
+                Broadcaster.broadcastToBot(
+                    new SendMessage(chatId,textField.getValue()));
+            getUI().access(()->postMessage("[" +LocalDateTime.now().format(formatter)+"] Вы: " + textField.getValue()));});
+            backToDialogs.addClickListener(e-> {
+                inChatWindow=false;
+                getUI().access(()->setContent(dialogsLayout));
+                Broadcaster.unregister(this);
+                detach();
+            });
+            enterTextLayout.addComponent(textField);
+            enterTextLayout.addComponent(sendButton);
+            enterTextLayout.addComponent(backToDialogs);
+            Broadcaster.register(this);
+            this.addComponent(enterTextLayout);
+        }
+
+
+        @Override
+        public void receiveBroadcast(Update update) {
+
+            getUI().access(() -> postMessage("["+LocalDateTime.now().format(formatter)+ "] " +update.getMessage().getFrom().getFirstName() + ": "
+                    + update.getMessage().getText()));
+        }
+
+        private void postMessage(String s) {
+
+        if (this.getComponentCount() > 10) {
+            this.removeComponent(this.getComponent(1));
+        }
+        this.addComponent(new HorizontalLayout(new Label(s)));
+
+
+    }
+
+
     }
 }
